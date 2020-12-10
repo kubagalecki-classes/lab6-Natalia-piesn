16af500ecf72ef89207c075ce9c1427c2a6b115e
Nataliapiesn <01123786@pw.edu.pl>
2020-12-10 21:28:53 +0000

    Task 1 completed.

--- lab6.cpp
+++ lab6.cpp
@@ -1,6 +1,22 @@
+#include <algorithm>
+#include <experimental/random>
 #include <iostream>
+#include <random>
+#include <vector>
+using namespace std;
 
 int main()
 {
-    puts("Hello, World!");
+    vector< int > v;
+    for (int i = 0; i < 7; i++) {
+        v.push_back(std::experimental::randint(1, 10));
+    }
+    for (const int& e : v) {
+        cout << e << " ";
+    }
+    cout << endl;
+    std::sort(v.begin(), v.end());
+    for (const int& e : v) {
+        cout << e << " ";
+    }
 }
