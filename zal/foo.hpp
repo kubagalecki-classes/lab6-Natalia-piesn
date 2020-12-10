4e9633224ace11ab02a5cf0a56880eee80d79a7a
Nataliapiesn <01123786@pw.edu.pl>
2020-12-10 22:58:11 +0000

    v01

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -9,14 +9,14 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-    transform(people.begin(), people.end(), ret_v.rbegin(), [&](Human& a) {
+    std::transform(people.begin(), people.end(), ret_v.rbegin(), [&](Human& a) {
         if (a.isMonster() == true) {
-            ret_v.emplace_back('n');
+            return 'n';
         }
         else {
-            ret_v.emplace_back('y');
+            return 'y';
         }
     });
-    for_each(people.rbegin(), people.rend(), [&](Human& a){a.birthday();});
+    std::for_each(people.rbegin(), people.rend(), [&](Human& a){a.birthday();});
     return ret_v;
 }
